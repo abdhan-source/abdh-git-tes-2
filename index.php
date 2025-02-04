0461d61fc8f41aedd83f64ebfee8707fd1b9a01b
abdhan-source <abdulhanan985@gmail.com>
2025-02-04 15:42:41 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote 2</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, veritatis placeat eius debitis cupiditate modi
+        accusantium odit, quas, quam harum id enim nam amet possimus adipisci ab aliquam fugiat? Nulla.</p>
+</body>
+
+</html>
