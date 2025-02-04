a20a150a122f8135e8e92b77382782df8916d4c0
abdhan-source <abdulhanan985@gmail.com>
2025-02-04 15:48:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, veritatis placeat eius debitis cupiditate modi
         accusantium odit, quas, quam harum id enim nam amet possimus adipisci ab aliquam fugiat? Nulla.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
